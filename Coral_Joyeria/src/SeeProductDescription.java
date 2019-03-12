4e8f710eeb4b4bd28909be5fc7f03510a4f3e1a2
Angel Limas <angel.eliseo@gmail.com>
2019-03-12 00:41:02 -0600

    changes added for domain under test

--- Coral_Joyeria/src/SeeProductDescription.java
+++ Coral_Joyeria/src/SeeProductDescription.java
@@ -1,6 +1,5 @@
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,8 +9,17 @@ public class SeeProductDescription {
 	public void test() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\Documents\\My Sandbox Software\\chromedriver_win32\\chromedriver.exe");	
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.google.com");
-		//driver.close();
+		driver.get("http://companymx.com/coral/");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//*[@id=\"blog\"]/div/div[1]/div[1]/a")).click();
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.quit();
 	}
 
 }
