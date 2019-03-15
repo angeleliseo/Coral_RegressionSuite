6fa5ecf02c0e76aa520611b0cd139af6b0a9d8d8
Angel Limas <angel.eliseo@gmail.com>
2019-03-15 02:47:44 -0600

    Started creating POM for coral page

---
+++ Coral_Joyeria/Page Objects/pageObjects/MainMenu.java
@@ -0,0 +1,22 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+
+public class MainMenu {
+	
+	private By lnkinicio;
+	private By lnkcontactos;
+	private By lnkcarrito;
+	private By lnkabout;
+	private By lnklogin;
+	private By txtsearch;
+	private By btnsearch;
+	private By combocategory;
+	private By combocriterio;
+	private By lnkcarrticon;
+	
+	public MainMenu(){
+		
+	}
+	
+}
